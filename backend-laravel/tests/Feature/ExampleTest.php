f1a79079dcaa380862dbd1208c82c481d877c0a6
Àlvaro Kumenius <akumenius@gmail.com>
2024-03-05 12:50:19 +0100

    Definit tests API Laravel

--- backend-laravel/tests/Feature/ExampleTest.php
+++ backend-laravel/tests/Feature/ExampleTest.php
@@ -18,4 +18,27 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_get_all_interpretes()
+    {
+        $response = $this->get('/api/interpretes');
+
+        $response->assertStatus(200)
+                ->assertJsonStructure([
+                    '*' => [
+                        'id',
+                        'nombre',
+                        'nacimiento',
+                        'numGoya'
+                    ]
+                ]);
+    }
+
+    public function test_interpretes_response_contains_penelope_cruz()
+    {
+        $response = $this->get('/api/interpretes');
+
+        $response->assertStatus(200)
+            ->assertJsonFragment(['id' => 1,'nombre' => 'Penélope Cruz', "nacimiento" => "1974-04-28", "numGoya" => 3]);        
+    }
 }
