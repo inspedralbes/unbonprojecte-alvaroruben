628e3fc8d1b66d78274051c361552ae10356a406
Alvaro Kumenius <akumenius@gmail.com>
2024-03-06 09:42:08 +0100

    COMMIT

---
+++ frontend-vue/src/views/LlistatView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      arrayItems: [],
+    }
+  },
+  created() {
+    this.getItems();
+  },
+  methods: {
+    async getItems() {
+
+      let url = "http://127.0.0.1:8000/api/interpretes";
+
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+
+      const jsonResponse = await response.json();
+      console.log("items", jsonResponse);
+      this.arrayItems = jsonResponse;    
+    },
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
